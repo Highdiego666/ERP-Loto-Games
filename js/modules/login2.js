acaac200eebfd60bf6fdf2984c9feb45a28bde0e
highdiego666 <highdiego666@hotmail.com>
2026-06-29 18:54:53 -0600

    Login completo con integración a DB y verificación de PIN

--- js/modules/login2.js
+++ js/modules/login2.js
@@ -10,8 +10,8 @@ window.verificarPIN = async (pin) => {
   // Usuarios incrustados directamente en la función
   const usuarios = [
     { id: 1, nombre: "Diego Perez", pin: "1620", rol: "admin", email: "admin@lotogames.com", privilegios: [] },
-    { id: 2, nombre: "Soporte Técnico", pin: "1111", rol: "soporte", email: "soporte@lotogames.com", privilegios: [] },
-    { id: 3, nombre: "Vendedor", pin: "2222", rol: "vendedor", email: "vendedor@lotogames.com", privilegios: [] },
+    { id: 2, nombre: "Charlie", pin: "2025", rol: "soporte", email: "soporte@lotogames.com", privilegios: [] },
+    { id: 3, nombre: "Alejandra", pin: "2222", rol: "vendedor", email: "vendedor@lotogames.com", privilegios: [] },
     { id: 4, nombre: "Técnico", pin: "3333", rol: "tecnico", email: "tecnico@lotogames.com", privilegios: [] }
   ];
   
